d572dd689457e726801c1594a64b43f2842827e2
dali14 <dalijlasii@gmail.com>
2022-06-20 03:16:24 +0200

    v8

--- components/VideoOne.js
+++ components/VideoOne.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ModalVideo from "react-modal-video";
 import BlockTitle from "./BlockTitle";
 import videoBG from "../assets/images/resources/video-bg-1-1.PNG";
-
+//hello
 const VideoOne = () => {
   const [open, setOpen] = useState({
     isOpen: false,
